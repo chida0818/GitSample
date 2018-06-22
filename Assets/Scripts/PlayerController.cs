bf39ecb59d197da051bb048987687790caa9ec7e
chida0818 <chida0818@yahoo.co.jp>
2018-06-22 10:02:01 +0900

    Player Controller
    Player Controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
 	void Start () {
 		
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(-0.5f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(0.5f, 0, 0);
+        }
+    }
 }
